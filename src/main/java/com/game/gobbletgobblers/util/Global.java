68cda4795ade9cd836b3579ea5e30be0bbbb26a9
Vanessa Hoamea <vanessahoamea@yahoo.com>
2023-11-06 12:57:06 +0200

    saving user preferences locally

--- src/main/java/com/game/gobbletgobblers/util/Global.java
+++ src/main/java/com/game/gobbletgobblers/util/Global.java
@@ -5,14 +5,16 @@ import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 
 import java.util.Objects;
+import java.util.prefs.Preferences;
 
 public class Global
 {
+    private static final Preferences prefs = Preferences.userRoot().node(Global.class.getName());
     private static final Media media = new Media(Objects.requireNonNull(Global.class.getResource("/com/game/gobbletgobblers/music/theme.mp3")).toString());
     private static final MediaPlayer mediaPlayer = new MediaPlayer(media);
-    private static boolean isMusicOn = true;
+    private static boolean isMusicOn = prefs.getBoolean("musicOn", true);
     private static boolean isMusicPlaying = false;
-    private static Color startingColor = Color.BLUE;
+    private static Color startingColor = Color.values()[prefs.getInt("startingColor", 0)];
 
     private Global() { }
 
@@ -39,6 +41,7 @@ public class Global
         mediaPlayer.play();
         isMusicOn = true;
         isMusicPlaying = true;
+        prefs.putBoolean("musicOn", true);
     }
 
     public static void stopMusic()
@@ -49,10 +52,12 @@ public class Global
         mediaPlayer.stop();
         isMusicOn = false;
         isMusicPlaying = false;
+        prefs.putBoolean("musicOn", false);
     }
 
     public static void switchStartingColor(Color startingColor)
     {
         Global.startingColor = startingColor;
+        prefs.putInt("startingColor", startingColor.ordinal());
     }
 }
